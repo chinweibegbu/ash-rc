db993dedd69f05d9f7c8db9f410f795f43b59272
chinweibegbu <chinwe.ibegbu@gmail.com>
2022-05-07 13:25:29 +0000

    Implemented User repository and controller

--- web/ASHRC/src/main/java/com/Ashesi/ASHRC/RepositoriesDAO/UserRepository.java
+++ web/ASHRC/src/main/java/com/Ashesi/ASHRC/RepositoriesDAO/UserRepository.java
@@ -3,8 +3,12 @@ package com.Ashesi.ASHRC.RepositoriesDAO;
 import com.Ashesi.ASHRC.Model.UserDetails;
 import org.springframework.data.repository.CrudRepository;
 import java.util.List;
-public interface UserRepository extends CrudRepository<UserDetails,Long> {
+import java.util.Optional;
+public interface UserRepository extends CrudRepository<UserDetails,Integer> {
 
     @Override
     List<UserDetails> findAll();
+
+    // Method to get a User by Email
+	Optional<UserDetails> getUserDetailsByEmail(String email);
 }
