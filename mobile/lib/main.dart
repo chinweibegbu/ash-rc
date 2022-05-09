9e33004bc97f9249f8fc20ab08cf2f864029f0e0
orla-ske <kodjoorlando@yahoo.co.uk>
2022-05-09 14:46:47 +0000

    added send functionality

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'ASH-RC',
       // ignore: prefer_const_constructors
-      home: ChatBotScreen(),
+      home: LoginPage(),
     );
   }
 }
